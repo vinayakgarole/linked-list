2781629c11a293dddeabf7934cd4c98c8a2adba6
vinayakgarole <vinayakgarole10@gmail.com>
2022-09-14 10:42:12 +0530

    added: Ability_to_create_Linked_List_by_adding_30_and_56_to_70

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,19 +1,26 @@
 package com.bridgelabz;
 public class LinkedList<T> {
     Node<T> head;
+
     public void add(T data) {
         Node<T> newNode = new Node<>();
         newNode.data = data;
-        if(head == null) {
+        if (head == null) {
             head = newNode;
-        }
-        else {
+        } else {
             Node<T> node = head;
             while (node.next != null) {
                 node = node.next;
             }
             node.next = newNode;
         }
+    }
+
+    public void addAtStart(T data) {
+        Node<T> newNode = new Node<>();
+        newNode.data = data;
+        newNode.next = head;
+        head = newNode;
     }
     public void print() {
         Node<T> node = head;
