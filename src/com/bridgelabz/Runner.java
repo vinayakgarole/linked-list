ce3e5b7b83c033ec7f3c45f7f44d8971879a72fc
vinayakgarole <vinayakgarole10@gmail.com>
2022-09-14 23:46:04 +0530

    added: Append 30 and 70 to 56

--- src/com/bridgelabz/Runner.java
+++ src/com/bridgelabz/Runner.java
@@ -2,11 +2,11 @@ package com.bridgelabz;
 public class Runner {
     public static void main(String[] args) {
         LinkedList<Integer> integerLinkedList = new LinkedList<>();
-        integerLinkedList.addAtStart(70);
+        integerLinkedList.add(56);
         integerLinkedList.print();
-        integerLinkedList.addAtStart(30);
+        integerLinkedList.add(30);
         integerLinkedList.print();
-        integerLinkedList.addAtStart(56);
+        integerLinkedList.add(70);
         integerLinkedList.print();
     }
 }
